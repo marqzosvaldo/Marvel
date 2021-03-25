b654974815bceac602c03ca1b1a772cdd3289037
marqzosvaldo <crosser_xr@hotmail.com>
2021-03-25 16:07:49 -0600

    Fonts

--- Marvel/Marvel/AssemblyInfo.cs
+++ Marvel/Marvel/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("FontAwesome5Regular.otf", Alias = "Font_Awesome_Solid")]
+[assembly: ExportFont("FontAwesome5Solid.otf", Alias ="Font_Awesome_Regular")]
+[assembly: ExportFont("FontAwesome5Brands.otf", Alias = "Font_Awesome_Brands")]
+
